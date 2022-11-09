26985e49278e5ed75f60b1de20e7bbb3b41c251d
LautaroGomez97 <95309149+LautaroGomez97@users.noreply.github.com>
2022-11-09 17:57:18 -0300

    push

--- Juego/proyectiles/proyectil.gd
+++ Juego/proyectiles/proyectil.gd
@@ -8,6 +8,7 @@ var velocidad:Vector2 = Vector2.ZERO
 var danio:float
 
 ## Metodos
+# warning-ignore:unused_argument
 func crear(pos: Vector2,dir: float, vel: float, danio_p: int) -> void:
 	position = pos
 	rotation = dir
